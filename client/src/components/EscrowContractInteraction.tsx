e83665eba70534b7c7e8320b00832f19e2d74486
sam <samt@samt.st>
2023-08-03 15:24:12 +0300

    allowances for escrow agreement in client

--- client/src/components/EscrowContractInteraction.tsx
+++ client/src/components/EscrowContractInteraction.tsx
@@ -160,6 +160,116 @@ const EscrowContractInteraction: React.FC = () => {
 		}
 	] as const
 
+	const ERC20ABI = [
+		{
+		  "constant": true,
+		  "inputs": [],
+		  "name": "name",
+		  "outputs": [{ "name": "", "type": "string" }],
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [],
+		  "name": "totalSupply",
+		  "outputs": [{ "name": "", "type": "uint256" }],
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [],
+		  "name": "decimals",
+		  "outputs": [{ "name": "", "type": "uint8" }],
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [{ "name": "_owner", "type": "address" }],
+		  "name": "balanceOf",
+		  "outputs": [{ "name": "balance", "type": "uint256" }],
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [
+			{ "name": "_to", "type": "address" },
+			{ "name": "_value", "type": "uint256" }
+		  ],
+		  "name": "transfer",
+		  "outputs": [],
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [
+			{ "name": "_spender", "type": "address" },
+			{ "name": "_value", "type": "uint256" }
+		  ],
+		  "name": "approve",
+		  "outputs": [],
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [
+			{ "name": "_owner", "type": "address" },
+			{ "name": "_spender", "type": "address" }
+		  ],
+		  "name": "allowance",
+		  "outputs": [{ "name": "", "type": "uint256" }],
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [
+			{ "name": "_from", "type": "address" },
+			{ "name": "_to", "type": "address" },
+			{ "name": "_value", "type": "uint256" }
+		  ],
+		  "name": "transferFrom",
+		  "outputs": [],
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [{ "name": "", "type": "address" }],
+		  "name": "balances",
+		  "outputs": [{ "name": "", "type": "uint256" }],
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [],
+		  "name": "symbol",
+		  "outputs": [{ "name": "", "type": "string" }],
+		  "type": "function"
+		},
+		{
+		  "inputs": [],
+		  "type": "constructor"
+		},
+		{
+		  "anonymous": false,
+		  "inputs": [
+			{ "indexed": true, "name": "from", "type": "address" },
+			{ "indexed": true, "name": "to", "type": "address" },
+			{ "indexed": false, "name": "value", "type": "uint256" }
+		  ],
+		  "name": "Transfer",
+		  "type": "event"
+		},
+		{
+		  "anonymous": false,
+		  "inputs": [
+			{ "indexed": true, "name": "owner", "type": "address" },
+			{ "indexed": true, "name": "spender", "type": "address" },
+			{ "indexed": false, "name": "value", "type": "uint256" }
+		  ],
+		  "name": "Approval",
+		  "type": "event"
+		}
+	  ] as const
+
 
 	// escrow contract 0x
 	const contractAddress = '0xC0D9E81c5FB957a7b93058Ae995Ee931d47DC903';
@@ -220,22 +330,41 @@ const EscrowContractInteraction: React.FC = () => {
 
 	const onSubmitAggreement = React.useCallback(async (event: { preventDefault: () => void; }) => {
 		event.preventDefault();
-
+	
 		try {
 			if (!!escrowContractRef.current) {
 				const initiatorCurrency = '0x36e6040b4186F9f0Ad9b3c25a9C1c9EE58112D0a'
 				const initiatorSuppliedAmount = ethers.parseEther("1")
 				const counterPartyCurrency = '0x540053115bA579EB32aCddfaFe2d121340553411'
 				const counterPartyRequiredAmount = ethers.parseEther("1000")
+	
+				// Get the current signer (user's wallet)
+				const signer = library.getSigner()
+	
+				// Create an instance of the ERC20 token contract you want to interact with
+				const tokenContract = new ethers.Contract(initiatorCurrency, ERC20ABI, signer)
+				// Here `abi` is the ABI of the ERC20 contract (you can import it from a separate file)
+	
+				// Check the current allowance
+				const allowance = await tokenContract.allowance(signer.getAddress(), escrowContractRef.current.address)
+	
+				// Calculate new allowance
+				const newAllowance = allowance.add(initiatorSuppliedAmount);
 
+				// Approve the new allowance
+				const approveTx = await tokenContract.approve(escrowContractRef.current.address, newAllowance)
+
+				// Wait for approval to be mined
+				await approveTx.wait()
+		
+				// Now, when the contract is approved, we can call the createAgreement function
 				const receipt = await escrowContractRef.current.createAgreement(
 					initiatorCurrency,
 					initiatorSuppliedAmount,
 					counterPartyCurrency,
 					counterPartyRequiredAmount,
-					{ value: ethers.parseEther("1")}
 				);
-
+	
 				console.log('receipt', receipt);
 			}
 		} catch (error) {
