dd7ad7e33dd3a51ceeba9a39d9d78444b76f76bb
sam <samt@samt.st>
2023-08-03 12:23:39 +0300

    display dummy buttons

--- client/src/components/EscrowAgreement.tsx
+++ client/src/components/EscrowAgreement.tsx
@@ -10,17 +10,22 @@ interface EscrowAgreementProps {
 const EscrowAgreement: React.FC<EscrowAgreementProps> = ({agreement, myAddress}) => {
   // component logic goes here
 
+  const isMyAgreement = agreement.initiator.initiatorAddress === myAddress
+	const { isFilled } = agreement
+
   return (
     <div className='card'>
-    	<div className='subtitle'>initiatorAddress: {agreement.initiator.initiatorAddress} {agreement.initiator.initiatorAddress === myAddress && <span className='neon-text'>(you)</span>}</div>
+    	<div className='subtitle'>initiatorAddress: {agreement.initiator.initiatorAddress} {isMyAgreement && <span className='neon-text'>(you)</span>}</div>
 		
     	<div className='subtitle'>offers</div> <span className='neon-text'>{agreement.initiator.suppliedAmount} {agreement.initiator.currency}</span> <br/>
 		
 		<br/><br/>
     	<div className='subtitle'>wants in return</div>
 		<span className='neon-text'>{agreement.counterparty.requiredAmount}  {agreement.counterparty.currency}</span>
-		<br/>
-		filled: {String(agreement.isFilled)}<br/>
+		<br/><br/>
+		{isMyAgreement && !isFilled && <button className='button'>[cancel]</button>}
+		{!isMyAgreement && !isFilled && <button className='button'>[fill]</button>}
+		{isFilled && <span>[filled]</span>}
     </div>
   );
 }
