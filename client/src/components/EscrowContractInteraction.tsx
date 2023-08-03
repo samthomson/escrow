9dbe5c323957faf54c0b61a9a34cd11306fd019a
sam <samt@samt.st>
2023-08-03 23:05:42 +0300

    approve amount

--- client/src/components/EscrowContractInteraction.tsx
+++ client/src/components/EscrowContractInteraction.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ethers } from 'ethers';
 import { useWeb3React } from '@web3-react/core'
+import { BigNumber } from '@ethersproject/bignumber'
 
 import * as Types from '../declarations'
 import EscrowAgreement from './EscrowAgreement'
@@ -330,13 +331,23 @@ const EscrowContractInteraction: React.FC = () => {
 
 	const onSubmitAggreement = React.useCallback(async (event: { preventDefault: () => void; }) => {
 		event.preventDefault();
+
+
+		const initiatorCurrency = '0x36e6040b4186F9f0Ad9b3c25a9C1c9EE58112D0a'
+		const initiatorSuppliedAmount = 1
+		const counterPartyCurrency = '0x540053115bA579EB32aCddfaFe2d121340553411'
+		const counterPartyRequiredAmount = 1000
+
 	
 		try {
 			if (!!escrowContractRef.current) {
-				const initiatorCurrency = '0x36e6040b4186F9f0Ad9b3c25a9C1c9EE58112D0a'
-				const initiatorSuppliedAmount = ethers.parseEther("1")
-				const counterPartyCurrency = '0x540053115bA579EB32aCddfaFe2d121340553411'
-				const counterPartyRequiredAmount = ethers.parseEther("1000")
+				// todo: get decimal amount intelligently
+				const biInitiatorSuppliedAmount = ethers.parseUnits(`${initiatorSuppliedAmount}`, 18)
+				const biCounterPartyRequiredAmount = ethers.parseUnits(`${counterPartyRequiredAmount}`, 18)
+
+				// const bnInitiatorSuppliedAmount = BigNumber.from(ethers.parseEther(`${initiatorSuppliedAmount}`))
+				// const bnCounterPartyRequiredAmount = BigNumber.from(ethers.parseEther(`${counterPartyRequiredAmount}`))
+
 	
 				// Get the current signer (user's wallet)
 				const signer = library.getSigner()
@@ -346,23 +357,36 @@ const EscrowContractInteraction: React.FC = () => {
 				// Here `abi` is the ABI of the ERC20 contract (you can import it from a separate file)
 	
 				// Check the current allowance
-				const allowance = await tokenContract.allowance(signer.getAddress(), escrowContractRef.current.address)
+				// console.log('contract', escrowContractRef?.current)
+				// console.log('contract address', escrowContractRef.current?.address)
+				const currentAllowance = BigNumber.from(await tokenContract.allowance(signer.getAddress(), escrowContractRef.current.target))
+
 	
 				// Calculate new allowance
-				const newAllowance = allowance.add(initiatorSuppliedAmount);
+				const newAllowance = ethers.formatUnits(currentAllowance.add(biInitiatorSuppliedAmount).toString(), 18);
+
+				const newAllowanceToString = newAllowance.toString()
+				const newAllowanceParsed = ethers.parseUnits(newAllowanceToString, 18)
+
+				console.log('approvals', {
+					currentAllowance, newAllowance, biInitiatorSuppliedAmount,
+					newAllowanceToString,
+					newAllowanceParsed
+				})
 
 				// Approve the new allowance
-				const approveTx = await tokenContract.approve(escrowContractRef.current.address, newAllowance)
+				const approveTx = await tokenContract.approve(escrowContractRef.current.target, newAllowanceParsed)
+
 
 				// Wait for approval to be mined
-				await approveTx.wait()
+				// await approveTx.wait()
 		
 				// Now, when the contract is approved, we can call the createAgreement function
 				const receipt = await escrowContractRef.current.createAgreement(
 					initiatorCurrency,
-					initiatorSuppliedAmount,
+					biInitiatorSuppliedAmount,
 					counterPartyCurrency,
-					counterPartyRequiredAmount,
+					biCounterPartyRequiredAmount,
 				);
 	
 				console.log('receipt', receipt);
