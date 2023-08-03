0f86e61fcf939ebd1b0b0358716b1d9faffef346
sam <samt@samt.st>
2023-08-03 12:27:10 +0300

    show numbers

--- client/src/components/EscrowAgreement.tsx
+++ client/src/components/EscrowAgreement.tsx
@@ -17,11 +17,11 @@ const EscrowAgreement: React.FC<EscrowAgreementProps> = ({agreement, myAddress})
     <div className='card'>
     	<div className='subtitle'>initiatorAddress: {agreement.initiator.initiatorAddress} {isMyAgreement && <span className='neon-text'>(you)</span>}</div>
 		
-    	<div className='subtitle'>offers</div> <span className='neon-text'>{agreement.initiator.suppliedAmount} {agreement.initiator.currency}</span> <br/>
+    	<div className='subtitle'>offers</div> <span className='neon-text'>{agreement.initiator.suppliedAmount.toString()}</span> x <span className='neon-text'>{agreement.initiator.currency}</span> <br/>
 		
 		<br/><br/>
     	<div className='subtitle'>wants in return</div>
-		<span className='neon-text'>{agreement.counterparty.requiredAmount}  {agreement.counterparty.currency}</span>
+		<span className='neon-text'>{agreement.counterparty.requiredAmount.toString()}</span>  of <span className='neon-text'>{agreement.counterparty.currency}</span>
 		<br/><br/>
 		{isMyAgreement && !isFilled && <button className='button'>[cancel]</button>}
 		{!isMyAgreement && !isFilled && <button className='button'>[fill]</button>}
